255d786c0ab8c9467467e0c63ce9cb44312cc2a9
longbai <longbai@corp.netease.com>
2017-01-09 10:11:18 +0800

    add baidutongji

---
+++ public/js/baidutongji/tongji.js
@@ -0,0 +1,7 @@
+var _hmt = _hmt || [];
+(function() {
+    var hm = document.createElement("script");
+    hm.src = "https://hm.baidu.com/hm.js?b21e73ac2699651467eca5e1b6357871";
+    var s = document.getElementsByTagName("script")[0];
+    s.parentNode.insertBefore(hm, s);
+})();
